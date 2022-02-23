1807b6fba9ff11580628f65c0aae06aacebd8e46
Geeknerd1337 <geeknerd1337@gmail.com>
2022-02-22 22:56:31 -0600

    Style WOrk, Intiial Setup

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -2,10 +2,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world!</h1>
+    <div className="Main">
+     <div className="Left">
+     <Category title="Info"/>
+      <Project title="About"/>
+      <Project title="Github"/>
+      <Project title="Contact"/>
+     <Category title="Games"/>
+      <Project title="Netscape Cybermind"/>
+      <Project title="Misc. Games"/>
+     <Category title="VFX"/>
+      <Project title="HD ME"/>
+      <Project title="DOT COM GUY"/>
+      <Project title="Chip In The Streets"/>
+     <Category title="Project Management"/>
+      <Project title="Eagle One"/>
+
+     </div>
+     <div className="Right">
+     </div>
     </div>
+    
   );
 }
+
+function Category(props){
+  return(
+    <div className="Title">
+    <h1 className="headers">{props.title}</h1>
+    </div>
+  )
+}
+
+function Project(props){
+  return(
+    <div className="Project">
+    <h2 className="headers">{props.title}</h2>
+    </div>
+  )
+}
+
 
 export default App;
