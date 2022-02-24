e31e1fb9b95f94709a7a301524a8a52088d20bca
Geeknerd1337 <geeknerd1337@gmail.com>
2022-02-23 18:35:00 -0600

    Set Up Tabs

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,29 +1,56 @@
-import './App.css';
+import './App.scss';
+import React, { useState } from "react";
 
 function App() {
+  const[tab, setTab] = useState("HEY");
+   
+  
   return (
     <div className="Main">
      <div className="Left">
      <Category title="Info"/>
-      <Project title="About"/>
-      <Project title="Github"/>
-      <Project title="Contact"/>
+      <Project title="About" onTabChange={setTab} tab={tab}/>
+      <Project title="Github" onTabChange={setTab} tab={tab}/>
+      <Project title="Contact" onTabChange={setTab} tab={tab} />
      <Category title="Games"/>
-      <Project title="Netscape Cybermind"/>
-      <Project title="Misc. Games"/>
+      <Project title="Netscape Cybermind" onTabChange={setTab} tab={tab}/>
+      <Project title="Misc. Games" onTabChange={setTab} tab={tab}/>
      <Category title="VFX"/>
-      <Project title="HD ME"/>
-      <Project title="DOT COM GUY"/>
-      <Project title="Chip In The Streets"/>
-     <Category title="Project Management"/>
-      <Project title="Eagle One"/>
-
+      <Project title="HD ME" onTabChange={setTab} tab={tab}/>
+      <Project title="DOT COM GUY" onTabChange={setTab}  tab={tab}/>
+      <Project title="Chip In The Streets" onTabChange={setTab} tab={tab}/>
+     <Category title="Project Management" onTabChange={setTab} tab={tab}/>
+      <Project title="Eagle One" onTabChange={setTab} tab={tab}/>
+    <h1>{tab}</h1>
      </div>
      <div className="Right">
+       <PageFactory tab={tab}/>
      </div>
     </div>
     
   );
+}
+
+function PageFactory(props){
+    switch(props.tab){
+      case "About":
+        return <AboutPage/>
+      break;
+      default:
+        return <EmptyPage/>
+    }
+}
+
+function AboutPage(){
+  return(
+    <h1>This is where will put some info</h1>
+  )
+}
+
+function EmptyPage(){
+  return(
+    <h1>This is an empty page</h1>
+  )
 }
 
 function Category(props){
@@ -35,10 +62,11 @@ function Category(props){
 }
 
 function Project(props){
+
   return(
-    <div className="Project">
+    <button className={props.tab == props.title ? "ProjectSelected" : "Project"} onClick={() => {props.onTabChange(props.title)}}>
     <h2 className="headers">{props.title}</h2>
-    </div>
+    </button>
   )
 }
 
